07abd4a5f8aad4c6c40f590325d658aeef605865
sss82941700 <sss82941700@gmail.com>
2025-08-06 22:13:14 +0800

    Create finance.js

---
+++ pages/pages/finance.js
@@ -0,0 +1,8 @@
+export default function Finance() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>財報上傳與圖表</h1>
+      <p>這裡待建構上傳與圖表功能。</p>
+    </main>
+  );
+}
