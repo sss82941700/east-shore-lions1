46f5c77265bcb261cc84c1a3200387c4bba3966f
sss82941700 <sss82941700@gmail.com>
2025-08-06 22:16:34 +0800

    Create calendar.js

---
+++ pages/calendar.js
@@ -0,0 +1,8 @@
+export default function Calendar() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>活動行事曆</h1>
+      <p>這裡待建構 FullCalendar 模組。</p>
+    </main>
+  );
+}
